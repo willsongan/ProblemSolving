7cf6720e96c334b4b54b60b47f4e107d16777a46
willsongan <willsongan30@gmail.com>
2021-10-07 22:48:40 +0700

    Problem 7

---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    public int point;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Ball>();
+        if (player != null)
+        {
+            player.currentScore += point;
+            gameObject.SetActive(false);
+        }
+    }
+}
